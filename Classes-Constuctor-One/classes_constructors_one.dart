d2526a804827f5377b1622a20ee39f90421dc5d0
mahmoud <mahmoudhabib.eng@gmail.com>
2023-08-28 23:11:42 +0300

    add late final

--- Classes-Constuctor-One/classes_constructors_one.dart
+++ Classes-Constuctor-One/classes_constructors_one.dart
@@ -7,5 +7,29 @@ class Car {
 
   // default constuctor
   // this one is inhereted by sub-classes
-  Car();
+  //Car();
+
+  // initializer List with named constuctor
+  Car(
+    this._carName,
+  ) : this._carColor = 'Blue';
+}
+
+// Immutable class
+class Person {
+  final String _name;
+  // late is like a setter
+  late String tall;
+  // means color will be initalize one time but later
+  // be careful this will add setter to the class ==> you can initialize color more than one time
+  // in the code and this will throw an exception
+  late final String color;
+  Person(this._name);
+}
+
+void main() {
+  Person person = Person('Mahmoud');
+  person.color = 'White';
+  person.color = 'Blue';
+  print(person.color);
 }
